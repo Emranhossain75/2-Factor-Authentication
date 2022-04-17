633769f42317d3d159bbb437c7a4a11c261d6dba
Emran <emran4058@gmail.com>
2022-04-17 23:18:40 +0600

    Add 2 factor authencation

---
+++ Google2factorAuthentication/ViewModel/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Google2factorAuthentication.ViewModel
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+    public class LoginModelDBContext : DbContext
+    {
+        public DbSet<LoginModel> LoginModels { get; set; }
+    }
+}
